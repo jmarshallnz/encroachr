16758e61228821dfd38f053b5f0c47208cc2fb98
Jonathan Marshall <jmarshall@never.you.mind>
2016-03-15 12:34:29 +1300

    adds total biodiversity and risk (the former is wrong at least)

--- encroachr.R
+++ encroachr.R
@@ -23,48 +23,57 @@ reps = 1:50
 methods = c("Box", "Linear", "Ribbon", "Vertical")
 
 for (num in nums) {
-  pdf(paste0("output_", num, ".pdf"), width=12, height=8)
-  par(mfrow=c(4,3), mar=c(1,4,2,2))
+  pdf(paste0("output_", num, ".pdf"), width=15, height=8)
+  par(mfrow=c(4,5), mar=c(1,4,2,2))
   for (size in sizes) {
     for (method in methods) {
       cat("Processing location", num, ",", size, "for method", method, "\n")
       p = list()
       s = list()
       a = list()
+      b = list()
+      r = list()
       for (rep in reps) {
         # biodiversity data is in the _epi file
         filename = paste(num, size, method, rep, sep="_")
 
         # main data is here...
         d2 = readMat(file.path("data", paste0(filename, ".mat")))$simulation.data
+        d2_epi = readMat(file.path("data", paste0(filename, "_epi.mat")))$biodiversity.data
 
-        p[[rep]] = unlist(lapply(d2["edge.patches",,], mean))
+        p[[rep]] = unlist(lapply(d2["edge.patches",,], sum))
         s[[rep]] = unlist(lapply(d2["shape",,], mean))
-        a[[rep]] = unlist(lapply(d2["size.patches",,], mean))
+        a[[rep]] = unlist(lapply(d2["size.patches",,], sum))
+        b[[rep]] = unlist(d2_epi["total.biodiversity",,])
+        r[[rep]] = unlist(d2_epi["total.risk",,])
       }
 
-      p_max = max(unlist(lapply(p, max)))
-      main = paste(method, num, size, sep="_")
-      plot(NULL, ylim=c(0,p_max), xlim = c(1,length(p[[1]])), xlab="", xaxt="n", ylab="Average perimeter", main=main)
-      lapply(p, lines)
+      # write a function!
+      plot_thing <- function(p, name) {
+        p_max = max(unlist(lapply(p, max)))
+        main = paste(method, num, size, sep="_")
+        plot(NULL, ylim=c(0,p_max), xlim = c(1,length(p[[1]])), xlab="", xaxt="n", ylab=name, main=main)
+        lapply(p, lines)
+      }
 
-      s_max = max(unlist(lapply(s, max)))
-      main = paste(method, num, size, sep="_")
-      plot(NULL, ylim=c(0,s_max), xlim = c(1,length(s[[1]])), xlab="", xaxt="n", ylab="Average solidity", main=main)
-      lapply(s, lines)
+      plot_thing(p, "Total perimeter")
+      plot_thing(s, "Average solidity")
+      plot_thing(a, "Total area")
+      plot_thing(b, "Total biodiversity")
+      plot_thing(r, "Total risk")
 
-      a_max = max(unlist(lapply(a, max)))
-      main = paste(method, num, size, sep="_")
-      plot(NULL, ylim=c(0,a_max), xlim = c(1,length(a[[1]])), xlab="", xaxt="n", ylab="Average area", main=main)
-      lapply(a, lines)
+      save_data <- function(x, file) {
+        x = as.data.frame(x)
+        names(x) = paste0("rep",1:ncol(x))
+        x = cbind(time=1:nrow(x), x)
+        write.csv(x, file, row.names=FALSE)
+      }
 
-      p = as.data.frame(p); names(p) <- paste0("rep",1:ncol(p)); p = cbind(time=1:nrow(p), p)
-      s = as.data.frame(s); names(s) <- paste0("rep",1:ncol(s)); s = cbind(time=1:nrow(s), s)
-      a = as.data.frame(a); names(a) <- paste0("rep",1:ncol(a)); a = cbind(time=1:nrow(a), a)
-
-      write.csv(p, paste0(paste("out/perim", num, size, method, sep="_"), ".csv"), row.names=FALSE)
-      write.csv(s, paste0(paste("out/shape", num, size, method, sep="_"), ".csv"), row.names=FALSE)
-      write.csv(a, paste0(paste("out/area", num, size, method, sep="_"), ".csv"), row.names=FALSE)
+      save_data(p, paste0(paste("out/perim", num, size, method, sep="_"), ".csv"))
+      save_data(s, paste0(paste("out/shape", num, size, method, sep="_"), ".csv"))
+      save_data(a, paste0(paste("out/area", num, size, method, sep="_"), ".csv"))
+      save_data(b, paste0(paste("out/biodiversity", num, size, method, sep="_"), ".csv"))
+      save_data(r, paste0(paste("out/risk", num, size, method, sep="_"), ".csv"))
     }
   }
   dev.off()
